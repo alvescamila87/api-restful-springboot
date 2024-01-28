fd9a399fe6f821d7225b573f0ada857596386c45
Camila <alvescamila87@gmail.com>
2024-01-28 16:53:02 -0300

    create put (update) and delete methods by products

--- src/main/java/com/camila/springboot/controllers/ProductController.java
+++ src/main/java/com/camila/springboot/controllers/ProductController.java
@@ -22,25 +22,46 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @PostMapping("/products")
+    @PostMapping("/products") //create
     public ResponseEntity<ProductModel> createProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDTO, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
-    @GetMapping("/products")
+    @GetMapping("/products") //read
     public ResponseEntity<List<ProductModel>> searchByAllProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/products/{id}") //read
     public ResponseEntity<Object> searchByIdProduct(@PathVariable(value = "id") UUID id) {
         Optional<ProductModel> productOptional = productRepository.findById(id);
         if(productOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
         return ResponseEntity.status(HttpStatus.OK).body(productOptional.get());
+    }
+
+    @PutMapping("/products/{id}") //update
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductRecordDTO productRecordDTO) {
+        Optional<ProductModel> productOptional = productRepository.findById(id);
+        if(productOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        var productModel = productOptional.get(); // não instancia um novo produto, pegar o product opcional que já existe pelo id
+        BeanUtils.copyProperties(productRecordDTO, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
+    @DeleteMapping("/products/{id}") //delete
+    public ResponseEntity<Object> deleteByIdProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productOptinal = productRepository.findById(id);
+        if(productOptinal.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productRepository.delete(productOptinal.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product was deleted successfully.");
     }
 
 }
