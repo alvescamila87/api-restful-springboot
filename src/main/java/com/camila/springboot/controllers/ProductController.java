818b5785439b2f19bbbde138ed176d28a4ccd120
Camila <alvescamila87@gmail.com>
2024-01-28 15:26:14 -0300

    create controller, dto, repository and post method

---
+++ src/main/java/com/camila/springboot/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.camila.springboot.controllers;
+
+import com.camila.springboot.dtos.ProductRecordDTO;
+import com.camila.springboot.models.ProductModel;
+import com.camila.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> createProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDTO, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+}
